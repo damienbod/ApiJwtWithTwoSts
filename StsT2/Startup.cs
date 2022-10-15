ff7eb1dc90aa814dbae8795ae7a8da75f07041dd
damienbod <damien_bod@hotmail.com>
2022-10-15 08:48:17 +0200

    remove old error handling

--- StsT2/Startup.cs
+++ StsT2/Startup.cs
@@ -150,7 +150,6 @@ public class Startup
         if (_environment.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
-            app.UseDatabaseErrorPage();
         }
         else
         {
