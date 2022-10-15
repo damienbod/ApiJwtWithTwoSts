a644d6101eca79dd1bb058f4b1d8bdc597645615
damienbod <damien_bod@hotmail.com>
2022-10-15 07:43:07 +0200

    Update packages

--- RazorPageOidcClient/ApiClient/AuthConfigurations.cs
+++ RazorPageOidcClient/ApiClient/AuthConfigurations.cs
@@ -2,7 +2,7 @@
 {
     public class AuthConfigurations
     {
-        public string StsServer { get; set; }
-        public string ProtectedApiUrl { get; set; }
+        public string StsServer { get; set; } = string.Empty;
+        public string ProtectedApiUrl { get; set; } = string.Empty;
     }
 }
