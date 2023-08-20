e654217d96b8a2c687ed7a1d1176ead505753e61
damienbod <damien_bod@hotmail.com>
2023-08-20 10:26:51 +0200

    Update STS

--- IdentityProvider/Controllers/HomeController.cs
+++ IdentityProvider/Controllers/HomeController.cs
@@ -17,11 +17,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     public IActionResult Error()
     {
         return View();
