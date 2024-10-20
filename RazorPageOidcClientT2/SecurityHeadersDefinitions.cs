c1fa65f91b3ff590364184a3ecfd8fdba5d40e50
damienbod <damien_bod@hotmail.com>
2024-10-20 13:37:19 +0200

    security headers performance

--- RazorPageOidcClientT2/SecurityHeadersDefinitions.cs
+++ RazorPageOidcClientT2/SecurityHeadersDefinitions.cs
@@ -4,9 +4,15 @@ namespace RazorPageOidcClient;
 
 public static class SecurityHeadersDefinitions
 {
+    private static HeaderPolicyCollection? policy;
+
     public static HeaderPolicyCollection GetHeaderPolicyCollection(bool isDev)
     {
-        var policy = new HeaderPolicyCollection()
+        // Avoid building a new HeaderPolicyCollection on every request for performance reasons.
+        // Where possible, cache and reuse HeaderPolicyCollection instances.
+        if (policy != null) return policy;
+
+        policy = new HeaderPolicyCollection()
             .AddFrameOptionsDeny()
             .AddContentTypeOptionsNoSniff()
             .AddReferrerPolicyStrictOriginWhenCrossOrigin()
